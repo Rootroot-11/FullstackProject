ca360660a6882a21a8e152c6d805c5adfe7122ab
george <heorhii.makeiev@gmail.com>
2022-02-17 20:30:52 +0300

    I'm making a query_builder for device, now can I sort devices from price and name

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//to do helmet, cors, fileUpload
+mongoose.connect('mongodb://localhost:27017/device_store')
+    .then(() => console.log('MongoDB has started ...')
+    )
+    .catch(e => console.log(e))
+
+app.listen(5000, () => {
+    console.log('App listen 5000');
+});
+
+const {userRouter, deviceRouter, authRouter} = require('./routes');
+
+app.use('/devices', deviceRouter);
+app.use('/auth', authRouter);
+app.use('/users', userRouter);
