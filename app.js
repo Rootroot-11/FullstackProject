185eabd1296d2ac2e8a21c8738617cb9fc810e01
george <heorhii.makeiev@gmail.com>
2022-06-12 20:13:04 +0300

    I'm making jwt service, middleware to checking password and fixed some routes.

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
-const cors = require('cors');
+// const cors = require('cors');
 
 const app = express();
+const PORT = process.env.PORT || 80
 
 mongoose.connect('mongodb://localhost:27017/device_store')
     .then(() => console.log('MongoDB has started ...')
     )
     .catch(e => console.log(e))
 
-app.use(cors({origin: 'http://localhost:3000'}))
+// app.use(cors({origin: 'http://localhost:3000'}))
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-//todo helmet, cors, fileUpload
 
-app.listen(5000, () => {
+
+app.listen(PORT, () => {
     console.log('App listen 5000');
 });
 
@@ -25,3 +26,11 @@ const {userRouter, deviceRouter, authRouter} = require('./routes');
 app.use('/devices', deviceRouter);
 app.use('/auth', authRouter);
 app.use('/users', userRouter);
+
+app.get('/', (req, res) => {
+    res.end('<h1>Home page</h1>')
+})
+
+app.get('/about', (req, res) => {
+    res.end('<h1>About page</h1>')
+})
